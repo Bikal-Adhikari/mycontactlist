8dc36ee66aed40f12aed8ad4bb97036053b55f71
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-19 13:45:54 +1100

    user data fetch and display from server

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const api = "https://randomuser.me/api/";
+let userList = [];
+const api = "https://randomuser.me/api/?results=6";
+
+const userListELm = document.getElementById("list");
 
 const fetchUsers = async () => {
   // const fetchUsers = () => {
@@ -17,11 +20,40 @@ const fetchUsers = async () => {
   //     });
 
   //async&await
-  const response = await fetch(api);
-  console.log(response);
 
-  const data = await response.json();
-  console.log(data.results);
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    userList = data.results;
+
+    display(userList);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchUsers();
+
+const display = (userList) => {
+  let usrCard = "";
+  console.log(userList);
+  userList.forEach((usr) => {
+    usrCard += `<div class="card flex-grow-1" style="width: 18rem">
+<img
+  class="card-img-top"
+  src="${usr.picture.large}"
+  alt="Card image cap"
+/>
+<div class="card-body">
+  <h5 class="card-title">${usr.name.title} ${usr.name.first} ${usr.name.last}</h5>
+  <ul class="list-unstyled">
+    <li>${usr.cell}</li>
+    <li>${usr.email}</li>
+    <li>${usr.location.street.number} ${usr.location.street.name} ${usr.location.city} ${usr.location.country} ${usr.location.postcode}</li>
+  </ul>
+</div>
+</div>`;
+  });
+  userListELm.innerHTML = usrCard;
+  document.getElementById("userCount").innerText = userList.length;
+};
